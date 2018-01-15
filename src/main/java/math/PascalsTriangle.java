8c4fcd2089406071565422673fcfe627f5b8b766
LiCola <361401376@qq.com>
2018-01-15 11:57:40 +0800

    Pascal三角打印

---
+++ src/main/java/math/PascalsTriangle.java
@@ -0,0 +1,48 @@
+package math;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by LiCola on 2018/1/15.
+ * https://leetcode.com/problems/pascals-triangle/description/
+ * 根据输入的行数，输出Pascal's-Triangle（帕斯卡三角形，杨辉三角）
+ * 如 numRows=5 输出：
+ * [1],
+ * [1,1],
+ * [1,2,1],
+ * [1,3,3,1],
+ * [1,4,6,4,1]
+ * Pascal三角性质：
+ * 1：正整数构成，左右对称，由1开始
+ * 2：第N行数字个数为N
+ * 3：除每行最左侧和最右侧都为1外，其他数由左上和右上两数之和。
+ */
+public class PascalsTriangle {
+
+  public static List<List<Integer>> generate(int numRows) {
+    if (numRows <= 0) {
+      return Collections.emptyList();
+    }
+
+    //构造二维数组
+    List<List<Integer>> list = new ArrayList<>(numRows);
+    for (int i = 0; i < numRows; i++) {
+      //遍历构造 每个子数组 数量可以确定
+      List<Integer> subList = new ArrayList<>(i + 1);
+      for (int j = 0; j <= i; j++) {
+        if (j == 0 || j == i) {
+          //最左/最右为1
+          subList.add(1);
+        } else {
+          //其他都左上+右上之和
+          List<Integer> upSubList = list.get(i - 1);
+          subList.add(upSubList.get(j-1)+upSubList.get(j));
+        }
+      }
+      list.add(subList);
+    }
+    return list;
+  }
+}
---
+++ src/test/java/math/PascalsTriangleTest.java
@@ -0,0 +1,23 @@
+package math;
+
+import static org.junit.Assert.*;
+
+import java.util.Collections;
+import java.util.List;
+import org.junit.Test;
+
+/**
+ * Created by LiCola on 2018/1/15.
+ */
+public class PascalsTriangleTest {
+
+  @Test
+  public void generate() {
+    assertEquals(Collections.emptyList(), PascalsTriangle.generate(0));
+
+    List<List<Integer>> lists = PascalsTriangle.generate(5);
+    for (List<Integer> subList : lists) {
+      System.out.println(subList);
+    }
+  }
+}
